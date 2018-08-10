ac6bcfa46109c6ff99422615ed6c963390f192e3
hanayashiki <494450105@qq.com>
2018-08-10 17:23:49 +0900

    Move docker

--- interact.sh
+++ interact.sh
@@ -1,2 +1,4 @@
-./path.sh
-docker build -t aspnetapp . && docker run -v $ASR_SERVICE_PATH/Tools/cvte:/usr/local/kaldi/egs/cvte -t -i --entrypoint /bin/bash aspnetapp:latest 
+#!/bin/bash
+source path.sh
+chmod +x $ASR_SERVICE_PATH/Tools/cvte/predict.sh
+docker build -t wangchenyu/aspnetapp . && docker run -v $ASR_SERVICE_PATH/Tools/cvte:/usr/local/kaldi/egs/cvte -t -i --entrypoint /bin/bash wangchenyu/aspnetapp:latest 
