a3ba6291e487393f5121018fb81b9ef79917c1c2
hanyashiki <494450105@qq.com>
2018-08-10 16:24:56 +0800

    Set Core

---
+++ Core/DecodeResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class DecodeResult
+    {
+        public string Text { get; set; }
+    }
+}
---
+++ Core/Decoder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace Core
+{
+    public interface Decoder
+    {
+        DecodeResult DecodeAsync(Stream waveStream);
+    }
+}
