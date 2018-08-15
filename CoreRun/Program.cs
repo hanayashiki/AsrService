78ab7a95e77acc82509bf4c97f3e3b38e5fc0ffb
hanyashiki <494450105@qq.com>
2018-08-15 21:10:38 +0800

    C# request test for c++ redis

--- CoreRun/Program.cs
+++ CoreRun/Program.cs
@@ -34,11 +34,33 @@ namespace CoreRun
             
         }
 
+        static async void TestRemote()
+        {
+            ThreadPool.SetMinThreads(250, 250);
+
+            IDecoder decoder = new AsyncDecoder();
+
+            byte[] mockWave = new byte[] { 1, 2, 3, 4 };
+            IList<Task<DecodeResult>> list = new List<Task<DecodeResult>>();
+            for (int i = 0; i < 200; i++)
+            {
+                Task<DecodeResult> task = decoder.DecodeAsync(new Speech { SpeechId = i.ToString(), Wave = mockWave });
+                //Thread.Sleep(2);
+                list.Add(task);
+            }
+            for (int i = 0; i < 200; i++)
+            {
+                DecodeResult result = await list[i];
+                Console.WriteLine(result.Text + "|" + result.Message);
+            }
+
+        }
+
         static void Main(string[] args)
         {
             var logCfg = new FileInfo(AppDomain.CurrentDomain.BaseDirectory + "log4net.config");
             XmlConfigurator.ConfigureAndWatch(LogManager.GetRepository(Assembly.GetCallingAssembly()), logCfg);
-            TestAsync();
+            TestRemote();
             Console.ReadKey();
         }
     }
