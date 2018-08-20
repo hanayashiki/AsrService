9256a571fc1546fcc42565f2a4b86267351f061c
hanyashiki <494450105@qq.com>
2018-08-20 13:54:53 +0800

    Add error message

--- Service/Controllers/DecodingController.cs
+++ Service/Controllers/DecodingController.cs
@@ -22,11 +22,25 @@ namespace Service.Controllers
             _decoder = decoder;
         }
         [HttpPost]
-        public async Task<string> Decode(IFormFile file)
+        public async Task<string> Decode(IFormFile wave)
         {
+            if (wave == null)
+            {
+                DecodeResult errorResult = new DecodeResult
+                {
+                    Id = "",
+                    Text = "",
+                    Message = "Usage: POST <host_name>/Decoding/Decode\n" +
+                                "Content-Disposition: form-data; name=\"wave\"; filename=\"your.wav\"\n" +
+                                "Content-Type: audio/x-wav\n\n" +
+                                "<binary_contents>"
+                };
+                return JsonConvert.SerializeObject(errorResult, Formatting.Indented);
+            }
+
             Speech speech = new Speech();
-            speech.Wave = new byte[file.Length];
-            file.CopyTo(new MemoryStream(speech.Wave));
+            speech.Wave = new byte[wave.Length];
+            wave.CopyTo(new MemoryStream(speech.Wave));
             speech.SpeechId = Convert.ToBase64String(new SHA1CryptoServiceProvider().ComputeHash(speech.Wave));
             DecodeResult result = await _decoder.DecodeAsync(speech);
             return JsonConvert.SerializeObject(result, Formatting.Indented);
