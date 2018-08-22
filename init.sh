d5f210000c426f968a0efb1c8c79233438a6221e
wang chenyu <494450105@qq.com>
2018-08-22 19:53:00 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # This is to be run once the service starts
-# start redis-server
 mkdir /app/Logs &> /dev/null
+# start redis-server
 nohup redis-server &> /app/Logs/redis.log &
+# start web interface
 nohup make start_web_service &>> /app/Logs/web.log &
+# start decoder
 nohup sh /app/Kaldi/run-service.sh &>> /app/Logs/kaldi.log &
 echo "Service starting..."
 
