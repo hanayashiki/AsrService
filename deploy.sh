1a165bfee6496390557d9f927691a1c2038fad43
wang chenyu <494450105@qq.com>
2018-08-22 19:51:57 +0800

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-source path.sh
-chmod +x $ASR_SERVICE_PATH/Tools/cvte/predict.sh
-docker build -t wangchenyu/aspnetapp . && docker run -v $ASR_SERVICE_PATH/Tools/cvte:/usr/local/kaldi/egs/cvte aspnetapp -d -p 8080:80 aspnetapp:latest
-
